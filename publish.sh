2c495dfa4699e1884ef85b096657b271543dd563
ruoyuklaus <klaus1201810802@gmail.com>
2022-10-10 00:23:30 +0800

    📝publish blog: 🏷️

--- publish.sh
+++ publish.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 current_branch=$(git branch --show-current)
-status=$(git status)
+
 chenged=$(git status | grep ".md" | sed 's/.md//;s/^\t*//' | tr '\n' ',' | sed 's/,$/\n/' | cut -b -20)
 message=${1:-📝publish blog: 🏷️ $chenged}
 
-echo $status
-echo $message
+
 
 git add .
 git commit -m "$message"
 
+echo $message
 
-
-# git push origin "$current"
+git push origin "$current"
