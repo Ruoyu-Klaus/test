8532bfceb7e1138624820c79ec509d029d737c2d
ruoyuklaus <klaus1201810802@gmail.com>
2022-10-10 00:35:54 +0800

    📝publish blog: 🏷️ modified:   test

--- publish.sh
+++ publish.sh
@@ -5,13 +5,13 @@ current_branch=$(git branch --show-current)
 chenged=$(git status | grep ".md" | sed 's/.md//;s/^\t*//' | tr '\n' ',' | sed 's/,$/\n/' | cut -b -20)
 message=${1:-📝publish blog: 🏷️ $chenged}
 
-echo $message
-
-if[[-n chenged]]
-
+if [[ ! $chenged ]]; then
+    echo '🈚 No markdown file changed ❗'
+    exit 1
+fi
 git add .
 git commit -m "$message"
 
 echo $message
 
-git push origin "$current"
+git push -u origin "$current"
