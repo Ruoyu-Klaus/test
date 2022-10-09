0dc04bda5f7658a84e27c695a9f643cd45c5dd9f
ruoyuklaus <klaus1201810802@gmail.com>
2022-10-10 00:24:07 +0800

    📝publish blog: 🏷️

--- publish.sh
+++ publish.sh
@@ -5,7 +5,9 @@ current_branch=$(git branch --show-current)
 chenged=$(git status | grep ".md" | sed 's/.md//;s/^\t*//' | tr '\n' ',' | sed 's/,$/\n/' | cut -b -20)
 message=${1:-📝publish blog: 🏷️ $chenged}
 
+echo $message
 
+if[[-n chenged]]
 
 git add .
 git commit -m "$message"
